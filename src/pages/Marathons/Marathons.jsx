7a466e4c3d8c5eb3c0b33c25cd58c599b4c165b8
RaihanIslamRauf <md.raufislam27@gmail.com>
2025-03-06 20:21:24 +0600

    added all marathons on the marathons page

--- src/pages/Marathons/Marathons.jsx
+++ src/pages/Marathons/Marathons.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import MarathonsCard from "../Home/MarathonsCard";
 
 
 const Marathons = () => {
+    const [marathons, setMarathons] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/marathons')
+            .then(res => res.json())
+            .then(data => setMarathons(data))
+            .catch(error => console.error("Error fetching marathons:", error));
+    }, []);
+
     return (
-        <div>
-            Marathons
+        <div className="mx-auto py-6 px-4">
+            <h1 className="text-4xl font-bold text-white text-center mb-8">All Marathons</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6'>
+                {marathons.map(marathon => (
+                    <MarathonsCard key={marathon._id} marathon={marathon} />
+                ))}
+            </div>
         </div>
     );
 };
